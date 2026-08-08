a251c1aaf44676e7dac57e86de42759b980ac2ef
Poonam Kumari sahu <sahupoonam1044@gmail.com>
2026-08-09 00:55:11 +0530

    Create ecommerce_analytics database and queries

---
+++ sql/database.sql
@@ -0,0 +1,18 @@
+
+-- Create Database
+CREATE DATABASE ecommerce_analytics;
+
+-- Select Database
+USE ecommerce_analytics;
+
+-- Show All Databases
+SHOW DATABASES;
+
+-- Show All Databases
+SHOW DATABASES;
+
+-- Show Current Database
+SELECT DATABASE();
+
+-- Show Tables
+SHOW TABLES;
